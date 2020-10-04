a6f69e248bb7c5b4580f68b35fb64a7674f34fe8
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-04 05:11:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+ function iterativeLog(array) {
+   array.forEach((element, index, array) => { 
+     console.log(`${index}: ${element}`);
+   });
+ }
+ 
+ function iterate(callback) {
+   var arr = [1, 2, 3];
+   arr.forEach(callback);
+   return arr;
+ }
+ 
+ function doToArray(array, callback) {
+   array.forEach(callback);
+ }
